daba8dddad873ca43316bea8f5b431c9cb14420a
lunacraz <lunacraz@gmail.com>
2014-12-04 15:17:42 -0500

    twitter javascript

---
+++ public/js/twitter.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+  $('#twittersearch form').on('submit', function(event){
+  	event.preventDefault();
+  	$.ajax({
+  		url: $(this).attr('action'),
+  		type: $(this).attr('method'),
+  		data: $(this).serialize(),
+  		dataType: 'JSON'
+  	})
+  	.done(function(response){
+  		console.log(response);
+  	})
+  	.fail(function(response){
+  		console.log(response);
+  	})
+  })
+});
