cde14483024640e505f573514a85ee684e081bb4
lunacraz <lunacraz@gmail.com>
2014-12-19 17:51:10 -0500

    Update with signout route

--- app/controllers/foursquare.rb
+++ app/controllers/foursquare.rb
@@ -3,3 +3,8 @@ get '/auth/foursquare/callback' do
 	session[:foursquare] = {info: request.env['omniauth.auth']['info'], credentials: request.env['omniauth.auth']['credentials']}
 	redirect '/'
 end
+
+get '/foursquare/signout' do
+	session[:foursquare] = nil
+	redirect '/'
+end
