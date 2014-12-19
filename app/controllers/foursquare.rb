819ee924267b890d5cc11d939f26b7148bbe5130
lunacraz <lunacraz@gmail.com>
2014-12-19 18:08:03 -0500

    Update the foursquare profile div

--- app/controllers/foursquare.rb
+++ app/controllers/foursquare.rb
@@ -1,5 +1,4 @@
 get '/auth/foursquare/callback' do
-	binding.pry
 	session[:foursquare] = {info: request.env['omniauth.auth']['info'], credentials: request.env['omniauth.auth']['credentials']}
 	redirect '/'
 end
