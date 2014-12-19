497f5a6f247558839a0422fd285c94725ac3ad75
lunacraz <lunacraz@gmail.com>
2014-12-19 17:44:34 -0500

    Update to views, and testing foursquare API callback

--- app/controllers/foursquare.rb
+++ app/controllers/foursquare.rb
@@ -1,4 +1,5 @@
 get '/auth/foursquare/callback' do
+	binding.pry
 	session[:foursquare] = {info: request.env['omniauth.auth']['info'], credentials: request.env['omniauth.auth']['credentials']}
 	redirect '/'
 end
